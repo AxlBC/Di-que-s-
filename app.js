dbe3861b2adbf334cf66ed6e570b9a8dbf8834e1
Ryan <rbenitez1469@gmail.com>
2024-03-16 02:48:37 -0600

    Actualizacion

--- app.js
+++ app.js
@@ -6,59 +6,49 @@ let siAltura = 0;
 let noAltura = 0;
 let noAnchura = 0;
 
+// Obtenemos todas las propiedades de los botones y el contenedor
+let btnSi = document.getElementById('btnSi');
+let btnNo = document.getElementById('btnNo');
+const contenedor = document.getElementById('contenedorBotones');
+
 // Botón Sí
 function funcionSi() {
     let btnSi = document.getElementById('btnSi');
     let btnNo = document.getElementById('btnNo');
-
-    // Agrandar boton Si
-    /*
-    siAnchura = btnSi.offsetWidth;
-    siAltura = btnSi.offsetHeight;
-
-    btnSi.style.height = (siAltura * 1.1) + 'px';
-    btnSi.style.width = (siAnchura * 1.1) + 'px';
-
-    console.log(`altura = ${siAltura}, anchura = ${siAnchura}`);
-    */
 }
 
 
 // Botón No
 function funcionNo() {
-    // altura = 434, anchura = 1280
-    let btnSi = document.getElementById('btnSi');
-    let btnNo = document.getElementById('btnNo');
-
-    const contenedor = btnSi.offsetParent;
-    
-
-
-
-
-
-
-
-
-
-
-
-    if (btnSi.offsetWidth < 1280) {
+    // Agrandamos el boton Si hasta alcanzar el 90% de espacio del div
+    if (btnSi.offsetWidth < (contenedor.offsetWidth * 0.90)) {
         siAltura = btnSi.offsetHeight;
         siAnchura = btnSi.offsetWidth;
 
-        btnSi.style.height = (siAltura * 1.50) + "px";
-        btnSi.style.width = (siAnchura * 1.50) + "px";
-    } else {
-        noAltura = btnNo.offsetHeight;
-        noAnchura = btnNo.offsetWidth;
+        btnSi.style.height = (siAltura * 1.08) + 'px';
+        btnSi.style.width = (siAnchura * 1.1) + 'px';
+    } 
+    // Lo ajustamos al 100% si sobrepasamos el 90% del espacio
+    else {
+        btnSi.style.width = '100%';
+        console.log(btnSi.offsetWidth);
 
-        console.log(noAltura);
-        console.log(noAnchura);
+        if (btnSi.offsetWidth > (contenedor.offsetWidth * 0.92)) {
+            btnNo.removeEventListener('click', funcionNo);
+            btnNo.addEventListener('mouseover', movimientoRandom);
 
-        if (noAltura > 20) {
-            btnNo.style.height = (noAltura * 0.9) + "px";
-            btnNo.style.width = (noAnchura * 0.9) + "px";
+            //btnNo.addEventListener('mouseover', movimientoRandom());
+            console.log('LLego hasta aqui')
         }
     }
 }
+
+function movimientoRandom() {
+    console.log('Ahi estas');
+    let min = 150;
+    btnNo.style.position = 'absolute';
+    btnNo.style.left = (Math.random() * (window.screen.width - min)) + 'px';
+    btnNo.style.top = (Math.random() * (window.screen.height - min)) + 'px';
+    console.log('se activa');
+}
+
