762f71f98b553c7f46ad71b3115a9be4527df027
Ryan <rbenitez1469@gmail.com>
2024-03-15 19:11:45 -0600

    Actualiza app.js

--- app.js
+++ app.js
@@ -1,12 +1,50 @@
+// Medidas botón sí
+let siAnchura = 0;
+let siAltura = 0;
 
+// Medidas botón sí
+let noAltura = 0;
+let noAnchura = 0;
 
 // Botón Sí
 function funcionSi() {
-    let valor = "";
+    let btnSi = document.getElementById('btnSi');
+    let btnNo = document.getElementById('btnNo');
+
+    // Agrandar boton Si
+    /*
+    siAnchura = btnSi.offsetWidth;
+    siAltura = btnSi.offsetHeight;
+
+    btnSi.style.height = (siAltura * 1.1) + 'px';
+    btnSi.style.width = (siAnchura * 1.1) + 'px';
+
+    console.log(`altura = ${siAltura}, anchura = ${siAnchura}`);
+    */
 }
 
 
 // Botón No
-function funcionSi() {
-    let valor = "";
+function funcionNo() {
+    // altura = 434, anchura = 1280
+    let btnSi = document.getElementById('btnSi');
+    let btnNo = document.getElementById('btnNo');
+
+    if (btnSi.offsetWidth < 1280) {
+        siAltura = btnSi.offsetHeight;
+        siAnchura = btnSi.offsetWidth;
+
+        btnSi.style.height = (siAltura * 1.50) + "px";
+        btnSi.style.width = (siAnchura * 1.50) + "px";
+    } else {
+        noAltura = btnNo.offsetHeight;
+        noAnchura = btnNo.offsetWidth;
+
+        console.log(noAltura);
+
+        if (noAltura > 20) {
+            btnNo.style.height = (noAltura * 0.9) + "px";
+            btnNo.style.width = (noAnchura * 0.9) + "px";
+        }
+    }
 }
