c67588f34097e36c5a032a69edb650d4ec91a817
Ryan <rbenitez1469@gmail.com>
2024-03-27 16:57:36 -0600

    Actualiza archivos

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+// Variables
+let contador = 0;
+const no = new Audio('resources/Pou-diciendo-no.mp3');
+
 // Medidas botón sí
 let siAnchura = 0;
 let siAltura = 0;
@@ -20,7 +24,24 @@ function funcionSi() {
 
 // Botón No
 function funcionNo() {
+    let intento = 0;
+
+    if (contador < 7) {
+        no.play();
+        contador++;
+        console.log(contador);
+    } else {
+        const tieneClickEvent = btnNo.onclick !== null;
+
+        if (tieneClickEvent) {
+            btnNo.removeEventListener('click', funcionNo);
+            btnNo.addEventListener('mouseover', movimientoRandom);
+        }      
+    }
+
+
     // Agrandamos el boton Si hasta alcanzar el 90% de espacio del div
+    /*
     if (btnSi.offsetWidth < (contenedor.offsetWidth * 0.90)) {
         siAltura = btnSi.offsetHeight;
         siAnchura = btnSi.offsetWidth;
@@ -41,6 +62,7 @@ function funcionNo() {
             console.log('LLego hasta aqui')
         }
     }
+    */
 }
 
 
@@ -51,6 +73,7 @@ function movimientoRandom() {
     btnNo.style.position = 'absolute';
     btnNo.style.left = (Math.random() * (window.screen.width - min)) + 'px';
     btnNo.style.top = (Math.random() * (window.screen.height - min)) + 'px';
+
+    no.play();
     console.log('se activa');
 }
-
